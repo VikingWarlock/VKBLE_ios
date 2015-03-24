5995b1257e663d94cffbc6f79b77f9a556fdd859
VikingWarlock <vikingwarlock@gmail.com>
2015-03-25 01:06:31 +0800

    init

---
+++ VKBLE_ios/VKBLE_ios/VKBLE_ios.h
@@ -0,0 +1,13 @@
+//
+//  VKBLE_ios.h
+//  VKBLE_ios
+//
+//  Created by viking warlock on 3/25/15.
+//  Copyright (c) 2015 viking warlock. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VKBLE_ios : NSObject
+
+@end
---
+++ VKBLE_ios/VKBLE_ios/VKBLE_ios.m
@@ -0,0 +1,13 @@
+//
+//  VKBLE_ios.m
+//  VKBLE_ios
+//
+//  Created by viking warlock on 3/25/15.
+//  Copyright (c) 2015 viking warlock. All rights reserved.
+//
+
+#import "VKBLE_ios.h"
+
+@implementation VKBLE_ios
+
+@end
